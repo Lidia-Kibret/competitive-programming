04de3cfb57c975d40c1a91035b6b64dcb9c8bef6
Lidia Kibret <lidiakibret4@gmail.com>
2026-05-31 09:11:42 +0300

    Time: 7 ms (63.9%), Space: 19.2 MB (53.77%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,11 +1,7 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
         if x < 0 or (x % 10 == 0 and x != 0):
             return False
         
-        s = str(x)
-        return s == s[::-1] 
+        nums = str(x)
+        return nums == nums[::-1]
