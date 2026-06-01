37ed798cf8b2a5778c4cae18558e8f8f60f99ac4
Lidia Kibret <lidiakibret4@gmail.com>
2026-06-01 21:06:59 +0300

    Time: 8 ms (26.64%), Space: 19.1 MB (98.34%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,15 +1,14 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        pairs = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000 }
+        pairs = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
         total = 0
         for i in range(len(s)):
             val = s[i]
             num = pairs[s[i]]
 
-            if i < len(s) - 1 and num < pairs[s[i+1]]:
+            if i < len(s) - 1 and num < pairs[s[i + 1]]:
                 total -= num
             else:
                 total += num
-        return total
-        
-        
+        return total 
+                
