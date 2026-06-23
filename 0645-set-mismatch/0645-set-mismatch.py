9b16c5b1dc0fb66bf6e64e9fbc9dfe08d7a8dab4
Lidia Kibret <lidiakibret4@gmail.com>
2026-06-23 21:53:53 +0300

    Time: 7 ms (86.83%), Space: 20.9 MB (39.83%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,23 +1,18 @@
-class Solution(object):
-    def findErrorNums(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        num_set = set()
-        duplicate = -1
+        res = set()
+        repeat = -1
 
-        
         for num in nums:
-            if num in num_set:
-                duplicate = num
-            num_set.add(num)
-
-        
-        for i in range(1, n + 1):
-            if i not in num_set:
+            if num in res:
+                repeat = num
+            res.add(num)
+            
+        for i in range(1 , n + 1):
+            if i not in res:
                 missing = i
                 break
 
-        return [duplicate, missing]
+        return [repeat, missing]
+        
