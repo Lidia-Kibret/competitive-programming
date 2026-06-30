b3958ad88133110220742330b2980db6a7cdf00b
Lidia Kibret <lidiakibret4@gmail.com>
2026-06-30 17:02:05 +0300

    Time: 3 ms (75.83%), Space: 19.4 MB (34.37%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [[set() for _ in range(3)] for _ in range(3)]
+
+        for i in range(9):
+            for j in range(9):
+                val = board[i][j]
+
+                if val == ".":
+                    continue
+
+               
+                if val in rows[i]:
+                    return False
+                rows[i].add(val)
+
+               
+                if val in cols[j]:
+                    return False
+                cols[j].add(val)
+
+                
+                bi, bj = i // 3, j // 3
+                if val in boxes[bi][bj]:
+                    return False
+                boxes[bi][bj].add(val)
+
+        return True
+        
