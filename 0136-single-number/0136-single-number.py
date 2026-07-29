5988c6cb7514e1d10f245adcb544adf7c88f7a6f
Lidia Kibret <lidiakibret4@gmail.com>
2026-07-29 22:27:23 +0300

    Time: 0 ms (100%), Space: 21.2 MB (43.18%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,14 +1,9 @@
-class Solution(object):
-    def singleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        counts = {}
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        answer = 0
+
         for num in nums:
-            counts[num] = counts.get(num, 0) + 1
-        
-        for num, count in counts.items():
-            if count == 1:
-                return num
+            answer ^= num
+
+        return answer
         
