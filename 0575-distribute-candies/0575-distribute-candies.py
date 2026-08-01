0facc76adb24318371caef6e1f6e184da3d523f1
Lidia Kibret <lidiakibret4@gmail.com>
2026-08-01 11:39:12 +0300

    Time: 20 ms (57.42%), Space: 21.2 MB (30.84%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.py
@@ -0,0 +1,6 @@
+class Solution:
+    def distributeCandies(self, candyType: List[int]) -> int:
+        unique = len(set(candyType))
+        return min(unique, len(candyType) // 2)
+        
+        
